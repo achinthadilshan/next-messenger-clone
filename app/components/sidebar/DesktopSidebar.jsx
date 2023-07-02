93757bd6b0395bd4d40dd0141b55637d9eda4b40
ACHii <>
2023-07-02 15:33:15 +0530

    update

--- app/components/sidebar/DesktopSidebar.jsx
+++ app/components/sidebar/DesktopSidebar.jsx
@@ -3,10 +3,13 @@
 import useRoutes from '@app/hooks/useRoutes'
 import { useState } from 'react'
 import DesktopItem from './DesktopItem'
+import Avatar from '../Avatar'
 
-const DesktopSidebar = () => {
+const DesktopSidebar = ({ currentUser }) => {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
+
+  console.log(currentUser)
 
   return (
     <div className="hidden justify-between lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:flex lg:w-20 lg:flex-col lg:overflow-y-auto lg:border-r-[1px] lg:bg-white lg:pb-4 xl:px-6">
@@ -23,6 +26,11 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col items-center justify-between">
+        <div onClick={() => setIsOpen(true)} className="cursor-pointer transition hover:opacity-75">
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   )
