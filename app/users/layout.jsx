598677d11bff2aac43f8b9a3aa845a3e4b867030
ACHii <>
2023-07-02 20:44:45 +0530

    update

--- app/users/layout.jsx
+++ app/users/layout.jsx
@@ -1,9 +1,15 @@
+import getUsers from '@app/actions/getUsers'
 import SideBar from '@app/components/sidebar/Sidebar'
+import UsersList from './components/UsersList'
 
 const UsersLayout = async ({ children }) => {
+  const users = await getUsers()
   return (
     <SideBar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UsersList items={users} />
+        {children}
+      </div>
     </SideBar>
   )
 }
